b8420b5b643ce190d5a6a1341b193a9876154497
annek <annek03@univie.ac.at>
2023-09-06 07:26:54 +0200

    Structure

---
+++ db/access.php
@@ -0,0 +1,15 @@
+<?php
+$capabilities = array(
+    'gradereport/gradinggroups:view' => array(
+        'riskbitmask' => RISK_PERSONAL,
+        'captype' => 'read',
+        'contextlevel' => CONTEXT_COURSE,
+        'legacy' => array(
+            'student' => CAP_ALLOW,
+            'teacher' => CAP_ALLOW,
+            'editingteacher' => CAP_ALLOW,
+            'manager' => CAP_ALLOW
+        )
+    ),
+);
+?>
---
+++ version.php
@@ -0,0 +1,4 @@
+<?php
+$plugin->version  = 2023083000;
+$plugin->requires = 2023083000;
+?>

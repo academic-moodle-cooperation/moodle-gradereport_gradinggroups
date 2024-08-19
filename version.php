d36c1d569a0689e8c1d985427265a681261d52fe
Martin Reichel <martin.w.reichel@univie.ac.at>
2024-08-19 08:01:15 +0000

    Update file version.php

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version  = 2024081901;
+$plugin->version  = 2024071701;
 $plugin->release = "v4.4.1";       // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2024042200;      // Requires this Moodle version!

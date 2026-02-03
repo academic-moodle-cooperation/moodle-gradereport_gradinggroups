569541b840a2abd2102da6f8801aab91c5f1af30
Anne <anne.kreppenhofer@tuwien.ac.at>
2026-02-03 17:52:21 +0100

    8485 update version
    Signed-off-by: Anne <anne.kreppenhofer@tuwien.ac.at>

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 
-$plugin->version  = 2025071600;
+1$plugin->version  = 2025071600.01;
 $plugin->release = "v5.0.0";       // User-friendly version number.
 $plugin->maturity = MATURITY_STABLE;
 $plugin->requires = 2025041400;      // Requires this Moodle version!
